92f560878426850d6831e23dabe0eed68ea8c128
Frumpy9 <32330391+Frumpy9@users.noreply.github.com>
2022-12-29 02:52:31 -0500

    added time stuff

--- src/main/startAll.lua
+++ src/main/startAll.lua
@@ -1,8 +1,20 @@
 console.clear()
 package.path = "./main/?.lua;"
 print("Starting Pokemon Fire Red inputs.")
+
+local startTime = os.time();
+print("Started Game at: " .. startTime)
 for i = 1, 9 do
+    local sTime = os.time();
+    print("Started " .. i .. " at: " .. sTime)
     local filename = string.format("%d.lua", i)
     local chunk = assert(loadfile(filename))
     chunk()
+    local eTime = os.time();
+    print("Ended " .. i .. " at: " .. eTime)
+    print("Total time for " .. i .. ": " .. (eTime - sTime) / 60 .. " minutes")
 end
+
+local endTime = os.time();
+print("Ended Game at: " .. endTime)
+print("Total Game time: " .. (endTime - startTime) / 60 .. " minutes")
